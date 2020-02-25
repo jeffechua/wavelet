89d42186882269d34b36adb8abf73dca4685b2e4
Jefferson <jeffe.chua@gmail.com>
2020-02-25 09:45:14 +0000

    Initial commit.

---
+++ Assets/Draw.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Draw : MonoBehaviour {
+
+	public float drawRadius;
+	public Color drawColor;
+
+	Texture2D tex;
+	int width;
+	int height;
+
+	Vector2 mousePosOld;
+
+	void Start() {
+		tex = (Texture2D)GetComponent<RawImage>().texture;
+		width = tex.width;
+		height = tex.height;
+		Clean();
+	}
+
+	void Update() {
+
+		switch (Input.inputString) {
+			case "1":
+				drawColor = new Color(1, 0, 0, 1);
+				break;
+			case "2":
+				drawColor = new Color(0, 1, 0, 1);
+				break;
+			default:
+				break;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			mousePosOld = LocalMousePosition;
+		} else if (Input.GetMouseButton(0)) {
+			Vector2 mousePos = LocalMousePosition;
+			int intDrawRadius = Mathf.CeilToInt(drawRadius);
+			for (Vector2 pos = mousePosOld; pos != mousePos; pos = Vector2.MoveTowards(pos, mousePos, 1))
+				for (int i = -intDrawRadius; i <= intDrawRadius; i++)
+					for (int j = -intDrawRadius; j <= intDrawRadius; j++)
+						if (Mathf.Sqrt(i * i + j * j) <= drawRadius)
+							tex.SetPixel(Mathf.RoundToInt(pos.x) + i, Mathf.RoundToInt(pos.y) + j, drawColor);
+			mousePosOld = mousePos;
+			tex.Apply();
+		}
+
+		drawRadius += Input.GetAxis("Mouse ScrollWheel");
+
+		if(Input.GetKeyDown(KeyCode.R)) {
+			Clean();
+		}
+
+	}
+
+	void Clean() {
+		tex.LoadRawTextureData(new byte[width * height * 16]);
+		tex.Apply();
+	}
+
+
+	Vector2 LocalMousePosition {
+		get => (Vector2)Input.mousePosition + GetComponent<RectTransform>().rect.min - (Vector2)transform.position;
+	}
+
+}
---
+++ Assets/WaveEngine.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class WaveEngine : MonoBehaviour {
+
+	public RawImage ICDisplay;
+	public RawImage systemDisplay;
+	RenderTexture systemDisplayTexture;
+
+	public ComputeShader waveCompute;
+	public Texture2D initialConditions;
+	public RenderTexture systemTexture;
+
+	public int width;
+	public int height;
+
+	public float c;
+	public float dt;
+	public int f;
+
+	int resetKernel;
+	int dispKernel;
+	int veloKernel;
+
+	Vector2 mousePosOld;
+
+	ComputeBuffer forceWaitBuffer1;
+	ComputeBuffer forceWaitBuffer2;
+
+	void Awake() {
+
+		systemTexture = new RenderTexture(width, height, 0, RenderTextureFormat.RFloat);
+		systemTexture.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
+		systemTexture.volumeDepth = 4;
+		systemTexture.enableRandomWrite = true;
+		systemTexture.Create();
+
+		initialConditions = new Texture2D(width, height, TextureFormat.RGFloat, false);
+		ICDisplay.texture = initialConditions;
+
+		resetKernel = waveCompute.FindKernel("Reset");
+		dispKernel = waveCompute.FindKernel("ComputeDisplacement");
+		veloKernel = waveCompute.FindKernel("ComputeVelocity");
+		waveCompute.SetTexture(resetKernel, "IC", initialConditions);
+		waveCompute.SetTexture(resetKernel, "System", systemTexture);
+		waveCompute.SetTexture(dispKernel, "System", systemTexture);
+		waveCompute.SetTexture(veloKernel, "System", systemTexture);
+
+		forceWaitBuffer1 = new ComputeBuffer(1, 4);
+		forceWaitBuffer2 = new ComputeBuffer(1, 4);
+		waveCompute.SetBuffer(dispKernel, "forceWait", forceWaitBuffer1);
+		waveCompute.SetBuffer(veloKernel, "forceWait", forceWaitBuffer2);
+
+		systemDisplayTexture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
+		systemDisplayTexture.Create();
+		systemDisplay.texture = systemDisplayTexture;
+
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			waveCompute.Dispatch(resetKernel, width / 8, height / 8, 1);
+		}
+		for (int i = 0; i < f; i++) {
+			int[] forceWaitResult = new int[1];
+			waveCompute.SetFloat("c2", c * c);
+			waveCompute.SetFloat("dt", dt);
+			waveCompute.Dispatch(veloKernel, width / 8, height / 8, 1);
+			forceWaitBuffer1.GetData(forceWaitResult);
+			waveCompute.Dispatch(dispKernel, width / 8, height / 8, 1);
+			forceWaitBuffer2.GetData(forceWaitResult);
+		}
+		Graphics.Blit(systemTexture, systemDisplayTexture, 0, 0);
+	}
+
+	private void OnDestroy() {
+		forceWaitBuffer1.Dispose();
+		forceWaitBuffer2.Dispose();
+	}
+
+}
